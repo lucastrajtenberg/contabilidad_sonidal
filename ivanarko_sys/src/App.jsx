9729ef68437184a999b5622690d4d92c527e1641
lucas <lucasjtraj@gmail.com>
2025-05-17 12:06:01 -0300

    add salas dropdown, logica para usar mapear las opciones desde los .json

--- ivanarko_sys/src/App.jsx
+++ ivanarko_sys/src/App.jsx
@@ -7,8 +7,8 @@ import alquilerData from "./data/alquiler.json";
 function App() {
   const [expandedRows, setExpandedRows] = useState({
     ensayo: true,
-    birras: true,
-    alquiler: true,
+    birras: false,
+    alquiler: false,
   });
 
   const toggleRow = (key) => { //Esto switchea el estado de los bichos expanded
@@ -20,9 +20,15 @@ function App() {
 
   const [selectedEnsayo, setSelectedEnsayo] = useState("ensayo");
 
+  const [selectedSala, setselectedSala] = useState("1");
+
   const handleEnsayoChange = (e) => {
     setSelectedEnsayo(e.target.value);
   };
+
+  const handleSalaChange = (e) => {
+    setselectedSala(e.target.value);
+  }
 
   const renderVentasGrid = (nombre) => {
     if (nombre === "ensayo") {
@@ -33,16 +39,16 @@ function App() {
             <td>
   <div className="dropdown-wrapper">
     <select className="dropdown-ventas" value={selectedEnsayo} onChange={handleEnsayoChange}>
-      <option value="ensayo">Ensayo</option>
-      <option value="ensayo trio">Ensayo trío</option>
-      <option value="ensayo duo">Ensayo dúo</option>
-      <option value="clase">Clase</option>
-      <option value="practica individual">Práctica individual</option>
+      {ensayoData.map(({ nombre }) => (
+        <option key={nombre} value={nombre}>
+          {nombre.charAt(0).toUpperCase() + nombre.slice(1)}
+        </option>
+      ))}
     </select>
     <span className="flecha-dropdown">&#9662;</span> {/* flechita ▼ en unicode */}
   </div>
 </td>
-              <td>${ensayoData.find(item => item.nombre === selectedEnsayo)?.precio || "$0"}</td>
+              <td>${ensayoData.find(item => item.nombre === selectedEnsayo)?.precio || "0"}</td>
               <td></td>
             </tr>
           </tbody>
@@ -81,7 +87,31 @@ function App() {
     }
   };
   
-  //render row renderea cada columna teniendo en cuenta si está expandida o colapsada ventas, checkeando el 
+  //render row renderea cada fila teniendo en cuenta si está expandida o colapsada ventas, checkeando el 
+
+  const renderHead = () => (
+        <tr>
+            <th className="col-nombre">
+              <form onSubmit={(e) => e.preventDefault()} className="form-nombre">
+                <input
+                  type="text"
+                  placeholder="nombre"
+                  className="input-nombre"
+                />
+              </form>
+              <div className="dropdown-wrapper">
+                <select className="dropdown-salas" value={selectedSala} onChange={handleSalaChange}>
+                  <option value="1">Sala 1</option>
+                  <option value="2">Sala 2</option>
+                  <option value="3">Sala 3</option>
+                </select>
+                <span className="flecha-dropdown">&#9662;</span> {/* flechita ▼ en unicode */}
+              </div>
+            </th>
+            <th className="col-ventas">Ventas</th>
+            <th className="col-total">Total</th>
+          </tr>
+  )
 
   const renderRow = (nombre) => (
     <tr key={nombre}>
@@ -107,20 +137,7 @@ function App() {
     <div className="tabla-container">
       <table className="tabla">
         <thead>
-          <tr>
-            <th className="col-nombre">
-              <form onSubmit={(e) => e.preventDefault()} className="form-nombre">
-                <span>Nombre:</span>
-                <input
-                  type="text"
-                  placeholder="nombre"
-                  className="input-nombre"
-                />
-              </form>
-            </th>
-            <th className="col-ventas">Ventas</th>
-            <th className="col-total">Total</th>
-          </tr>
+          {renderHead()}
         </thead>
         <tbody>
           {renderRow("ensayo")}
