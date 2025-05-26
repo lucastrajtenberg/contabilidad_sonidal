e5436b3894815aa71210b74b69311649f3427201
lucas <lucasjtraj@gmail.com>
2025-05-26 18:32:30 -0300

    milagro x2. ahora si anda tailwind en serio

--- ivanarko_sys/src/App.jsx
+++ ivanarko_sys/src/App.jsx
@@ -3,16 +3,12 @@ import TablaCliente from "./components/TablaCliente";
 export default function App() {
   return (
     <div className="flex h-screen">
-  <div className="w-[80%] p-4 overflow-auto">
-    <TablaCliente />
-  </div>
-  <div className="w-[20%] p-4 text-2xl text-red-600 bg-gray-50 flex items-center justify-center">
-  Panel derecho
-</div>
-
-
-
-</div>
-
+      <div className="w-[80%] p-4 overflow-auto">
+        <TablaCliente />
+      </div>
+      <div className="w-[20%] p-4 text-2xl text-red-600 bg-gray-50 flex items-center justify-center">
+        Panel derecho
+      </div>
+    </div>
   );
 }
