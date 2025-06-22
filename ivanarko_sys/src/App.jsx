08f40f025a30833c082a16fe29ff96fc5ba9dcf1
lucastrajtenberg <lucasjtraj@gmail.com>
2025-06-22 20:53:20 -0300

    feat agregar clientes para que no sea tan local la cosa viste

--- ivanarko_sys/src/App.jsx
+++ ivanarko_sys/src/App.jsx
@@ -23,7 +23,9 @@ export default function App() {
     removeClient
   } = useStore();
 
-  console.log(clients);
+  const saveClients = async () => {
+
+  }
 
   useEffect(() => {
     try {
@@ -69,7 +71,7 @@ export default function App() {
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='es'>
-      <div className='flex h-full overflow-y-auto '>
+      <div className='flex h-dvh overflow-y-auto '>
         {/* NAVBAR */}
         <aside className='w-full max-w-[400px] h-full p-4 bg-grey-600 flex flex-col gap-4'>
           <div className='bg-black text-white p-4 rounded-md'>
